a2b622b28589609bda8f07d9c0639de07967b15b
Emmanuel Makau <emmanuelmakau90@gmail.com>
2025-02-02 12:10:07 +0300

    Updated frontend with modern UI and skeleton loader

--- backend/scraper/models.py
+++ backend/scraper/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.CharField(max_length=50)  # Store as string to keep formatting
+    image = models.URLField()
+    link = models.URLField()
+
+    def __str__(self):
+        return self.title
