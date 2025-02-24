1b26728d40e7c8f45ac115e32943483e574bc1e3
Emmanuel Makau <emmanuelmakau90@gmail.com>
2025-02-24 13:29:11 +0300

    added changes

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Footer from "../components/footer";
+import Navbar from "../components/navbar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -19,9 +20,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Navbar/>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
+      <Footer/>
     </html>
   );
 }
--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,5 +1,6 @@
+"use client"
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const NavBar = () => {
   const router = useRouter();
