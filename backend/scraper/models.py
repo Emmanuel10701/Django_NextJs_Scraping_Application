be471ac110dd626a67a3c98db187a72a2996c151
Emmanuel Makau <emmanuelmakau90@gmail.com>
2025-03-03 20:10:31 +0300

    added the models

--- backend/scraper/models.py
+++ backend/scraper/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='products/', blank=True, null=True)  # Allows optional images
     price = models.DecimalField(max_digits=10, decimal_places=2)
     date_posted = models.DateTimeField(auto_now_add=True)
     quantity = models.PositiveIntegerField()
