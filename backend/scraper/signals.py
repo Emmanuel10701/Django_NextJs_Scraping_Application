4b82dda7c9cef56269074a55334ad59eff3f2667
Emmanuel Makau <emmanuelmakau90@gmail.com>
2025-03-04 10:07:48 +0300

    Added signals for job and product notifications

--- backend/scraper/signals.py
+++ backend/scraper/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.conf import settings
-from .models import Product
+from .models import Product, Job
 
 @receiver(post_save, sender=Product)
 def notify_admin_on_new_product(sender, instance, created, **kwargs):
@@ -11,8 +11,24 @@ def notify_admin_on_new_product(sender, instance, created, **kwargs):
         admin_users = User.objects.filter(is_superuser=True).values_list("email", flat=True)
         if admin_users:
             subject = "New Product Added!"
-            message = f"A new product has been added:\n\nTitle: {instance.title}\nPrice: {instance.price}\n\nCheck it out!"
-            
+            message = f"A new product has been added:\n\nName: {instance.name}\nPrice: {instance.price}\n\nCheck it out!"
+
+            send_mail(
+                subject,
+                message,
+                settings.EMAIL_HOST_USER,  # From email
+                list(admin_users),  # To email (list of superuser emails)
+                fail_silently=False,
+            )
+
+@receiver(post_save, sender=Job)
+def notify_admin_on_new_job(sender, instance, created, **kwargs):
+    if created:
+        admin_users = User.objects.filter(is_superuser=True).values_list("email", flat=True)
+        if admin_users:
+            subject = "New Job Posted!"
+            message = f"A new job has been posted:\n\nTitle: {instance.title}\nCompany: {instance.company}\nPayment: {instance.payment}\n\nCheck it out!"
+
             send_mail(
                 subject,
                 message,
