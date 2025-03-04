960ba44de4dddb67ea5202dc9600d6a099aa581a
Emmanuel Makau <emmanuelmakau90@gmail.com>
2025-03-04 10:09:35 +0300

    Added URLs for scraping products and jobs

--- backend/scraper/urls.py
+++ backend/scraper/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import get_scraped_data, get_scraped_jobs  # Importing views
 
 urlpatterns = [
+    path("scrape/products/", get_scraped_data, name="scrape-products"),
+    path("scrape/jobs/", get_scraped_jobs, name="scrape-jobs"),
 ]
 
 if settings.DEBUG:
